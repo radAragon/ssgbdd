ed3fb1d627f9029feec4cc0a16e9e5a8294ae66f
Radamés Aragón <rad.aragon@gmail.com>
2015-10-14 00:49:10 -0300

    Inclui mais opções de menu
    Adiciona menu Criar tabela e submenu respectivo.
    
    * Método criar coluna não implementado ainda.

--- ssgbdd.py
+++ ssgbdd.py
@@ -1,14 +1,17 @@
 #! /usr/bin/python3
 import multiprocessing
 import sqlite3
+import signal
 
 
 def db_instance(id, conn):
-    print('[%d] Iniciando instancia de banco' % id)
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    print('[b%d] Iniciando instancia de banco' % id)
     db = sqlite3.connect(':memory:')
     cur = db.cursor()
     cur.execute('''SELECT 1''')
-    print('[%d] Testando conexao: ' % id, cur.fetchone())
+    print('[b%d] Testando conexao: ' % id, cur.fetchone())
     conn.send(True)
     while True:
         try:
@@ -18,19 +21,65 @@ def db_instance(id, conn):
             if cmd == 'X':
                 break
         except Exception as e:
-            print('[%d] Erro:', e)
+            print('[b%d] Erro:', e)
             break
-    print('[%d] Encerrando conexao' % id)
+    print('[b%d] Encerrando conexao' % id)
     db.close()
+
+def menu_criar_col(cmd):
+    raise NotImplemented()
+    pass
+
+def menu_criar(cmd):
+    name_index = 1
+    try:
+        if cmd[1] == 'TABELA':
+            name_index += 1
+        table_name = cmd[name_index]
+    except IndexError:
+        print('Falta parametro: CRIAR [nome_tabela]')
+        return
+
+    print('Tabela:', table_name)
+    print('Comandos: [C]oluna, [R]egra, [P]ronto, [D]esistir')
+
+    submenu = {
+        'C': menu_criar_col,
+    }
+
+    subcmd = None
+    while subcmd != 'D':
+        try:
+            subcmd = input('... ').upper().split()
+            submenu[subcmd[0]](subcmd)
+        except KeyError:
+            pass
+        except IndexError:
+            pass
+        except KeyboardInterrupt:
+            break
+
+    if cmd == 'D':
+        return
 
 
 print('''
 Trabalho de SGBDD
 Simples Sistema de Gerenciamento de Banco de Dados Distribuido
 ''')
-inst_num = input('Instancias: ')
+
+while True:
+    try:
+        i = input('Instancias: ')
+        inst_num = int(i)
+        break
+    except ValueError:
+        pass
+    except KeyboardInterrupt:
+        exit(1)
+
 instances = list()
-for n in range(0, int(inst_num)):
+for n in range(inst_num):
     instances.insert(n, dict())
     instances[n]['id'] = n
     instances[n]['conn'], child_conn = multiprocessing.Pipe()
@@ -44,18 +93,29 @@ for n in instances:
         pass
 
 print('''
-Comandos: SAIR
+Comandos: CRIAR, SAIR
 ''')
 
-while True:
+menu = {
+    'CRIAR': menu_criar,
+}
+
+cmd = None
+while cmd != 'SAIR':
     #recebe instrucao
-    cmd = input('> ')
-    if cmd == 'SAIR':
-        print('Finalizando')
+    try:
+        cmd = input('> ').upper().split()
+        menu[cmd[0]](cmd)
+    except KeyError:
+        pass
+    except IndexError:
+        pass
+    except KeyboardInterrupt:
         break
 
+print('Finalizando')
 for n in instances:
     n['conn'].send('X')
     n['proc'].join()
 
-print('Fim.')
+print('Fim\n')

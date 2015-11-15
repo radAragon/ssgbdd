6221e1bbc3217d4beb38beeb3e7bbf2a9cc7c438
radamesaragon <radames@alexandriadigital.com.br>
2015-11-15 17:23:15 -0200

    Implementa método testa_query e faz referência única a DB

--- metabanco.py
+++ metabanco.py
@@ -1,33 +1,62 @@
-metabanco = None
+DB = None
 
-def estrutura_metadados(db):
-    cur = db.cursor()
+
+def estrutura_metadados(connection):
+    global DB
+    DB = connection
+    cur = DB.cursor()
     cur.execute('''
-    CREATE TABLE IF NOT EXISTS tabela (
+    CREATE TABLE IF NOT EXISTS tabelas (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         tabela_nome TEXT
     )''')
     print('PRONTA TABELAS')
     cur.execute('''
-    CREATE TABLE IF NOT EXISTS coluna (
+    CREATE TABLE IF NOT EXISTS colunas (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        tabela_id TEXT,
+        tabelas_id TEXT,
         coluna_nome TEXT,
         coluna_tipo TEXT
     )''')
     print('PRONTA COLUNAS')
     cur.execute('''
-    CREATE TABLE IF NOT EXISTS regra (
+    CREATE TABLE IF NOT EXISTS regras (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        tabela_id INTEGER,
-        coluna_id INTEGER,
+        tabelas_id INTEGER,
         site_id INTEGER,
+        colunas_id INTEGER,
         criterio TEXT
     )''')
+    DB.commit()
     print('PRONTA REGRAS')
-    global metabanco
-    metabanco = db
 
 
-def cria_regras(cmd):
-    pass
+def testa_sql(create_query):
+    cur = DB.cursor()
+    cur.execute(create_query)
+    print('SQL avaliado com sucesso')
+    return None
+
+
+def cria_meta_tabela(create_query):
+    query_parts = create_query.partition('(')
+    create_table = query_parts[0].split()
+    table_name_index = 2
+    if (create_table[1] in ['TEMP', 'TEMPORARY']):  # temporary table
+        table_name_index += 1
+    if (create_table[table_name_index] == 'IF'):  # if not exists
+        table_name_index += 3
+    table_name = create_table[table_name_index]
+    schema_format = table_name.split('.')
+    if (len(schema_format) > 1):  # schema name
+        table_name = schema_format[1]
+    print('Criando tabela %s' % table_name)
+    cur = DB.cursor()
+    cur.execute('''
+    INSERT INTO tabelas (tabela_nome)
+    VALUES (:nome)
+    ''', {
+        nome: table_name,
+    })
+    print(cur.lastrowid)
+    return None
